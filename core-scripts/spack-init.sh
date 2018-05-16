8555e35c5cece21f97214b0ef01d34d1dbbec83e
Daniel Topa <dantopa@lanl.gov>
2018-05-16 17:38:17 -0600

    reformat spack.sh

--- core-scripts/spack-init.sh
+++ core-scripts/spack-init.sh
@@ -2,7 +2,7 @@
 printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 # #  A L I A S
-alias sp='cd ${SPACK_ROOT}; pwd'
+alias sp="cd ${SPACK_ROOT}; pwd"
 
 alias scgcm="spack config get compilers"
 alias scgcn="spack config get config"
@@ -11,15 +11,15 @@ alias scgmo="spack config get modules"
 alias scgpa="spack config get packages"
 alias scgre="spack config get repos"
 
-export dir_yaml="${SPACK_ROOT}/etc/spack/defaults/"
+export dir_yaml="${SPACK_ROOT}/etc/spack/defaults"
 
-alias   vcm="vi ${dir_yaml}compilers.yaml"
-alias   vcn="vi ${dir_yaml}config.yaml"
-alias   vmi="vi ${dir_yaml}mirrors.yaml"
-alias   vmo="vi ${dir_yaml}modules.yaml"
-alias   vpa="vi ${dir_yaml}packages.yaml"
-alias  vhcm="vi ~/.spack/compilers.yaml"
-alias vhlcm="vi ~/.spack/linux/compilers.yaml"
+alias   vcm="vi ${dir_yaml}/compilers.yaml"
+alias   vcn="vi ${dir_yaml}/config.yaml"
+alias   vmi="vi ${dir_yaml}/mirrors.yaml"
+alias   vmo="vi ${dir_yaml}/modules.yaml"
+alias   vpa="vi ${dir_yaml}/packages.yaml"
+alias  vhcm="vi ${HOME}/.spack/compilers.yaml"
+alias vhlcm="vi ${HOME}/.spack/linux/compilers.yaml"
 
 ## openmpi
 alias    sfo="spack find openmpi"
@@ -28,6 +28,7 @@ alias sfoldf="spack find -ldf openmpi"
 ## modules
 alias reft="echo 'spack module refresh --delete-tree --module-type tcl';  spack module refresh --delete-tree --module-type tcl"
 alias refl="echo 'spack module refresh --delete-tree --module-type lmod'; spack module refresh --delete-tree --module-type lmod"
+
 alias mu="echo 'module load lmod'; module load lmod; echo 'module use ${SPACK_ROOT}/share/spack/lmod/$(spack arch)/Core'; module use ${SPACK_ROOT}/share/spack/lmod/$(spack arch)/Core"
 alias fix_module_path="ep; echo ''; module unuse ${SPACK_ROOT}/share/spack/modules/$(spack arch); ep; echo ''; module use ${SPACK_ROOT}/share/spack/lmod/$(spack arch)/Core/; ep; echo ''"
 
