bcb7e64d9b1033ada1b401b14b7bc16be76dcdd3
Daniel Topa <dantopa@lanl.gov>
2018-05-16 16:18:29 -0700

    LLNL bash adjustments

--- core-scripts/modules.sh
+++ core-scripts/modules.sh
@@ -3,9 +3,8 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 alias ml="module list"
 alias ma="module available"
-alias ms="module show ${1}"
 
-alias maa="module -t --redirect avail | grep ${1}"
+# alias maa="module -t --redirect avail | grep ${1}"
 
 # dump available modules in a file
 export my_log="${id}/modules_available.txt"
@@ -13,7 +12,7 @@ export my_log="${id}/modules_available.txt"
 echo "modules available " >  ${my_log}
 date                      >> ${my_log}
 echo ""                   >> ${my_log}
-module available          >> ${my_log}  2>&1
+module available          >> ${my_log} 2>&1
 
 # dump loaded modules in a file
 export my_log="${id}/modules_list.txt"
