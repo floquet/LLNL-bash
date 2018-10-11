ab52a0eb7d42d97a66f54641bffeaef4ec6fd84d
Daniel Topa <dantopa@lanl.gov>
2018-10-10 18:51:39 -0600

    bookmarks: bitbucket or github

--- core-scripts/bookmarks.sh
+++ core-scripts/bookmarks.sh
@@ -22,49 +22,21 @@ if [ -d "${repos}" ]; then
     fi
 
     if [ -d "${repos}/Github" ]; then
-        export    github="${repos}/GitHub"
+        export    github="${repos}/Github"
         export llnl_bash="${github}/LLNL-bash"
         alias   gogithub="cd ${github}; pwd"
-        alias gollnlbash="cd ${llnl_bash}; pwd"
+        alias     gobash="cd ${llnl_bash}; pwd"
     fi
 
-    if [ -d "${repos}/master-lap" ]; then
-        export         lap="${repos}/master-lap"
-        export    lap_bash="${lap}/init/bash"
+    if [ -d "${bitbucket}/LLNL-lap" ]; then   #  #  #  LLNL only
+        export         lap="${bitbucket}/LLNL-lap"
         export lap_scripts="${lap}/scripts"
-        alias     golap="cd ${lap}; pwd"
-        alias  goseelap="cd ${lap}/${host_name}/${partition}; pwd"
+        alias        golap="cd ${lap}; pwd"
+        alias     goseelap="cd ${lap}/${host_name}/${partition}; pwd"
       fi
 
-    if [ -d "${repos}/master-ompi" ]; then
-        export      ompi="${repos}/master-ompi"
-        export ompi_bash="${ompi}/init/bash"
-        alias     goompi="cd ${ompi}; pwd"
-        alias  goseeompi="cd ${ompi}/${host_name}/${partition}; pwd"
-    fi
-
-    if [ -d "${repos}/node-queries" ]; then
-        export node_queries="${repos}/node-queries"
-        alias gonode="cd ${node_queries}; pwd"
-
-        export       dir_control="${node_queries}/control"
-        export         dir_sweep="${dir_control}/sweep"
-        export   dir_batch_files="${dir_control}/batch-files"
-        export  dir_batch_output="${dir_control}/batch-output"
-        export dir_batch_scripts="${dir_control}/batch-scripts"
-        alias gocontrol="cd ${dir_control};       pwd"
-        alias   gosweep="cd ${dir_sweep};         pwd"
-        alias      gobf="cd ${dir_batch_files};   pwd"
-        alias      gobo="cd ${dir_batch_output};  pwd"
-        alias      gobs="cd ${dir_batch_scripts}; pwd"
-    fi
-
-    if [ -d "${repos}/Open-MPI-development" ]; then
-        alias godevOMPI="cd Open-MPI-development; pwd"
-    fi
-
-    if [ -d "${repos}/Performance_Benchmarking" ]; then
-      export dir_performance="${repos}/Performance_Benchmarking"
+    if [ -d "${bitbucket}/Performance_Benchmarking" ]; then
+      export dir_performance="${bitbucket}/Performance_Benchmarking"
       export   dir_topa_bash="${dir_performance}/topa/spack/bash"
       export        dir_wsdb="${dir_performance}/topa/wsdb"
       alias goperformance="cd ${dir_performance}; pwd"
@@ -72,27 +44,28 @@ if [ -d "${repos}" ]; then
       alias        gowsdb="cd ${dir_wsdb}; pwd"
     fi
 
+    if [ -d "${github}/node-queries" ]; then
+        export node_queries="${github}/node-queries"
+        alias        gonode="cd ${node_queries}; pwd"
+
+        export       dir_control="${node_queries}/control"
+        export         dir_sweep="${dir_control}/sweep"
+        export   dir_batch_files="${dir_control}/batch-files"
+        export  dir_batch_output="${dir_control}/batch-output"
+        export dir_batch_scripts="${dir_control}/batch-scripts"
+        alias          gocontrol="cd ${dir_control};       pwd"
+        alias            gosweep="cd ${dir_sweep};         pwd"
+        alias               gobf="cd ${dir_batch_files};   pwd"
+        alias               gobo="cd ${dir_batch_output};  pwd"
+        alias               gobs="cd ${dir_batch_scripts}; pwd"
+    fi
+
+    if [ -d "${repos}/Open-MPI-development" ]; then
+        alias godevOMPI="cd Open-MPI-development; pwd"
+    fi
+
     if [ -d "${repos}/shuttle" ]; then
         alias goshuttle="cd ${repos}/shuttle; pwd"
-    fi
-
-    if [ -d "${repos}/snapshots" ]; then
-        alias gosnap="cd ${repos}/snapshots; pwd"
-    fi
-
-    if [ -d "${repos}/spack_tools" ]; then
-        alias gost="cd ${repos}/spack_tools; pwd"
-    fi
-
-    if [ -d "${repos}/Github" ]; then
-        alias gogithub="cd ${repos}/GitHub; pwd"
-    fi
-
-    if [ -d "${repos}/Bitbucket" ]; then
-        alias gobitbucket="cd ${repos}/Bitbucket; pwd"
-        if [ -d "${repos}/Bitbucket/bottom" ]; then
-            alias gobottom="cd ${repos}/Bitbucket/bottom"
-        fi
     fi
 
 fi
