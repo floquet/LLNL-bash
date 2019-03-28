9e961898017439ede25d2b09c5b8e86f86948704
Daniel Topa <dantopa@lanl.gov>
2019-03-28 08:14:35 -0700

    rzansel-power9-rzansel40 Thu Mar 28 08:14:34 PDT 2019

--- core-scripts/spack.sh
+++ core-scripts/spack.sh
@@ -4,5 +4,5 @@ printf '%s\n' "$(date), $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 alias l-spack=". ${core}/spack-init.sh"
 
 # # errands
-alias  gospack="cd ${mySpack}; pwd"
-alias gospackp="echo '. share/spack/setup-env.sh';. share/spack/setup-env.sh; echo 'l-spack'; l-spack"
+alias   gospack="cd ${mySpack}; pwd"
+alias initspack="echo '. share/spack/setup-env.sh';. share/spack/setup-env.sh; echo 'l-spack'; l-spack"
