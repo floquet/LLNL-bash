8dd2c4666862f306a7251e12d04e8d94779f496f
Daniel Topa <dantopa@lanl.gov>
2019-06-05 22:01:44 -0600

    modified:   utility-scripts/updater.sh

--- utility-scripts/updater.sh
+++ utility-scripts/updater.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 printf '%s\n' "+++  $(pwd)/${BASH_SOURCE[0]} $(date)"
+
+rsync --dry-run --update -raz --progress . /Users/l127914/Documents/repos/github/LLNL-bash/core-scripts
+
+rsync --update -raz --progress . /Users/l127914/Documents/repos/github/LLNL-bash/core-scripts
